5ba2809fada39856d3eb76be24257f34319b2fc4
Sakunori <hamu.714@gmail.com>
2020-08-11 13:56:50 +0900

    ChatSpace-ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,64 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html = `<div class="MessageField__message-list">
+                    <div class="MessageField__message-list__log">
+                      <div class="MessageField__message-list__log__member">
+                        ${message.user_name}
+                      </div>
+                      <div class="MessageField__message-list__log__time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="MessageField__message-list__comment">
+                      <p class="Message__content">
+                        ${message.content}
+                      </p>
+                      <img class="Message__image" src="${message.image}">
+                    </div>
+                  </div>`
+                  return html;
+    } else {
+      let html = `<div class="MessageField__message-list">
+                    <div class="MessageField__message-list__log">
+                      <div class="MessageField__message-list__log__member">
+                        ${message.user_name}
+                      </div>
+                      <div class="MessageField__message-list__log__time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="MessageField__message-list__comment">
+                      <p class="Message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+                  return html;
+    }
+  }
+  $('.main-chat__message-formid').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      let html = buildHTML(message)
+      $('.MessageField').append(html)
+      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.main-chat__message-formid__input__send-btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+      $('.main-chat__message-formid__input__send-btn').prop('disabled', false);
+    })
+  })
+});
